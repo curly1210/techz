793f1d42c5efaf8b3a3299ffcd427327fad1845c
Cuong Pham <1210.curly@gmail.com>
2024-11-01 21:04:19 +0700

    the first up

---
+++ database/migrations/2024_11_01_042116_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('order_time');
+            $table->integer('total_price');
+            $table->foreignId('shipment_id')->constrained('shipments');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('status_order_id')->constrained('status_orders');
+            $table->foreignId('payment_id')->constrained('payments');
+            $table->foreignId('code_sale_id')->constrained('code_sales');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Trang chủ nè !
+</div>
