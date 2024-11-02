dc9f5d65c2f4e31aafd0e3dbf5cdaf095518d6c5
Pham Nam <phamnamgenzno1@gmail.com>
2024-11-02 15:30:11 +0700

    create model,controller resource,route (categories)

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriesController;
 
 Route::get('/', [HomeController::class, 'index'])->name('name');
+Route::prefix('admin')->group(function () {
+    Route::resource('categories', CategoriesController::class);
+});
